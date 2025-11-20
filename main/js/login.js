1776a40d2b7fee939788244f44da6dd8c90f8873
kaua <kaua.santanaj@gmail.com>
2025-11-20 00:10:57 -0300

    dark mode implementado no login

--- main/js/login.js
+++ main/js/login.js
@@ -1,6 +1,6 @@
 let btnLogin = document.getElementById('btnLogin');
 
-
+// validation login
 btnLogin.addEventListener('click', function(){
 
 let emailValueLogin = document.getElementById('loginEmail').value  
@@ -19,4 +19,28 @@ if(emailValueLogin === email && passwordValueLogin === password){
     alert('Login successful!');
     window.location.href = 'homePage.html'
 }
+});
+
+
+
+let button = document.getElementById('btn-darkMode');
+let isDark = false; 
+
+// dark mode
+button.addEventListener('click', function(){
+
+if(isDark){
+button.textContent = 'White Mode';
+button.classList.remove('btn-white');
+button.classList.remove('btn-dark');
+document.body.classList.remove('dark-mode');
+}
+else{
+    button.textContent = 'Dark Mode';
+    button.classList.remove('btn-dark');
+    button.classList.add('btn-white');
+    document.body.classList.add('dark-mode');
+}
+
+isDark = !isDark;
 });
